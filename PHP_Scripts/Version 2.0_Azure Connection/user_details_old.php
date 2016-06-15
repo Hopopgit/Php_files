1fab9c220de3742d123f85efc532d0bb88e0117f
HARISANKAR <harisankarit@gmail.com>
2016-06-15 15:23:54 +0530

    Latest Php Files
    PHP files with Azure connection

---
+++ PHP_Scripts/Version 2.0_Azure Connection/user_details_old.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'connection.php';
+header('Content-Type:application/json');
+
+class User {
+	
+	private $db;
+	private $connection;
+	function __construct() {
+		$this->db = new DB_Connection();
+		$this->connection = $this->db->get_connection();
+	}
+	
+	public function user_validation($mobile_number,$encrypt_password) {
+		$query ="select * from user_details where mobile_number='$mobile_number' and password='$encrypt_password'";
+		$result=mysqli_query($this->connection, $query);
+		if (mysqli_num_rows($result)>0) {
+			$json['success']='welcome back';
+			echo json_encode($json);
+			mysqli_close($this->connection);
+		}else {
+			echo json_encode("Details Mismatch");
+			mysqli_close($this->connection);
+		}
+	}
+}
+
+$User = new User();
+if (isset($_POST['mobile_number'],$_POST['password'])) {
+	$mobile_number = $_POST['mobile_number'];
+	$password = $_POST['password'];
+	
+	if (!empty($mobile_number) && !empty($password)) {
+		$encrypt_password=md5($password);
+		//echo $encrypt_password;
+		$User->user_validation($mobile_number,$encrypt_password);
+	}else {
+		echo json_encode("Enter full details please");
+	}
+}
+?>
